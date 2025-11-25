e2a8c0d7e9f6da22313556b6e00192390695e5a6
Roberto Kennedy <roberto.kpp@gmail.com>
2025-11-25 17:51:44 -0500

    *********

--- avançado/31/main.js
+++ avançado/31/main.js
@@ -1,1 +1,44 @@
-import "./js/produtos.js"
+const product = [
+  {
+    name: "Mouse",
+    price: 156.5,
+    amount: 15,
+  },
+  {
+    name: "Teclado",
+    price: 556.5,
+    amount: 12,
+  },
+  {
+    name: "Fone",
+    price: 600.0,
+    amount: 34,
+  },
+];
+
+const ul = document.querySelector("ul");
+
+function addIten(productList) {
+  for (let i = 0; i < productList.length; i++) {
+    const li = document.createElement("li");
+    const spanName = document.createElement("span");
+    const spanPrice = document.createElement("span");
+
+    spanName.textContent = product[i].name;
+    spanPrice.textContent = `Preço: ${product[i].price}`;
+    const select = document.createElement("select");
+    
+    for (let j = 0; j <= product.amount; j++) {
+      const option = document.createElement("option");
+      option.textContent = product[j].amount;
+      select.append(option);
+    }
+
+    ul.append(li);
+    li.append(spanName);
+    li.append(spanPrice);
+    li.append(select);
+  }
+}
+
+addIten(product);
