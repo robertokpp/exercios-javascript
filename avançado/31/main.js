5a705970befb362d2fabd4162efdb792d66e033b
Roberto Kennedy <roberto.kpp@gmail.com>
2025-11-27 17:15:54 -0500

    **

--- avançado/31/main.js
+++ avançado/31/main.js
@@ -23,27 +23,29 @@ const product = [
 
 function addIten(productList) {
   const listIten = document.getElementById("list-item");
-
   productList.forEach((item, index) => {
     const li = document.createElement("li");
     const spanName = document.createElement("span");
     const spanPrice = document.createElement("span");
-    const select = document.createElement("select");
     const button = document.createElement("button");
 
     spanName.textContent = item.name;
     spanPrice.textContent = `Preço: ${item.price}`;
     button.textContent = "Adicionar ao carrinho";
 
-    for (let j = 1; j <= item.amount; j++) {
-      const option = document.createElement("option");
-      option.textContent = j;
-      option.value = j;
-      select.append(option);
-    }
+    let select = atualizarSelect(item.amount);
 
     button.addEventListener("click", () => {
       addcarrinho(index, select.value);
+      let NewAmount = product[index].amount - select.value;
+      product[index].amount = NewAmount;
+
+      if (product[index].amount >= 0) {
+        select.remove();
+        let newSelect = atualizarSelect(product[index].amount);
+        li.insertBefore(newSelect, button);
+        select = newSelect;
+      }
     });
 
     li.append(spanName);
@@ -56,24 +58,38 @@ function addIten(productList) {
 
 addIten(product);
 
+function atualizarSelect(valorQtd) {
+  let select = document.createElement("select");
+  for (let j = 1; j <= valorQtd; j++) {
+    const option = document.createElement("option");
+    option.textContent = j;
+    option.value = j;
+    select.append(option);
+  }
+  return select;
+}
+
 function addcarrinho(item, qtd) {
+  const arrayCar = [];
   const aside = document.querySelector("aside");
   const lisCar = document.getElementById("list-car");
-
   const li = document.createElement("li");
-  const spanName = document.createElement("spam");
+  const spanName = document.createElement("span");
   const spanPrice = document.createElement("span");
-  const spanQtd = document.createElement("span")
+  const spanQtd = document.createElement("span");
+  
+  if (arrayCar.includes(product[item])) {
 
-  spanName.textContent = product[item].name;
-  spanPrice.textContent = product[item].price;
-  spanQtd.textContent = qtd;
+    spanQtd.textContent += qtd;
 
+  } else {
 
-  aside.append(lisCar);
-  lisCar.append(li);
-  li.append(spanName,spanPrice, spanQtd)
+    spanName.textContent = product[item].name;
+    spanPrice.textContent = product[item].price;
+    spanQtd.textContent = qtd;
 
-  console.log(product[item]);
-  console.log(qtd);
+    aside.append(lisCar);
+    lisCar.append(li);
+    li.append(spanName, spanPrice, spanQtd);
+  }
 }
