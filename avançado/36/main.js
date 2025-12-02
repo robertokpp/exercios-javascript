9571ad1604c70a30c966351b21499e107b9f32af
Roberto Kennedy <roberto.kpp@gmail.com>
2025-12-02 17:55:13 -0500

    36 ok

---
+++ avançado/36/main.js
@@ -0,0 +1,43 @@
+const opcaoMaquina = ["🪨", "📜", "✂️"];
+const SelectOption = document.querySelector("select");
+const btnEnviar = document.getElementById("jogar");
+const span = document.querySelector("span")
+const p = document.querySelector("p");
+
+function maquinaPlay(jogador) {
+  let random = opcaoMaquina[Math.floor(Math.random() * opcaoMaquina.length)];
+  
+  
+  span.textContent = `${jogador} X ${random}`
+
+  gamePlay(jogador, random);
+}
+
+function gamePlay(jogador, maquina) {
+  console.log(jogador, maquina);
+  if (jogador === "pedra" && maquina === "tesoura") {
+    p.textContent = "Voçe venceu"
+    return;
+  }
+  if (jogador === "papel" && maquina === "pedra") {
+    p.textContent = "Voçe venceu"
+    return;
+  }
+  if (jogador === "tesoura" && maquina === "papel") {
+    p.textContent = "Voçe venceu"
+    return;
+  }
+  if (jogador === maquina) {
+    p.textContent = "Empate"
+    return;
+  } else {
+    p.textContent = "Voçe Perdeu"
+    return;
+  }
+}
+
+btnEnviar.addEventListener("click", () => {
+  let optionChosen = SelectOption.value;
+  p.textContent= "";
+  maquinaPlay(optionChosen);
+});
